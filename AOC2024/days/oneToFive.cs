2811f9a5c94788f82256615c2daa1226eea538ab
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2024-12-02 13:37:57 +0800

    Quick restructure to OOP
    Will group the days into groups of 5 days in a class with the functions being DayOne, DayTwo etc..

---
+++ AOC2024/days/oneToFive.cs
@@ -0,0 +1,129 @@
+using System;
+using System.ComponentModel;
+using Microsoft.VisualBasic.FileIO;
+
+public class OneToFive
+{
+    public void DayOne()
+    {
+        // The input fields
+        string[] files = {"./assets/AOC2024.1.Input-1.csv", "assets/AOC2024.1.Input-2.csv" };
+
+        // List of lists of ints to use for storage
+        List<List<int>> lists = new List<List<int>>();
+        lists.Add(new List<int>());
+        lists.Add(new List<int>());
+
+        // Iterate over each file and process the data
+        for (int i = 0; i < files.Length; i++)
+        {
+            // Get the file to work with
+            using (TextFieldParser parser = new TextFieldParser(files[i]))
+            {
+                // Parse the text field into comma delimited
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                
+                // Iterate over each piece of data until we reach the end
+                while (!parser.EndOfData)
+                {
+                    // Process the text into an array of strings
+                    string[] fields = parser.ReadFields();
+                    // Iterate over each string
+                    foreach (string field in fields)
+                    {
+                        // Convert it to an int
+                        int fieldInt = Int32.Parse(field);
+                        // Add the int to the relevant list (i)
+                        lists[i].Add(fieldInt);
+                    }
+                }
+            }
+            // Sort the list now that we have all values
+            lists[i].Sort();
+        }
+
+        // Quick validity check to make sure we can troubleshoot a problem
+        if (lists[0].Count != lists[1].Count) { Console.WriteLine("List length mismatch!"); }
+
+        // List of distances so we can keep track of our data points
+        List<int> distances = new List<int>();
+        // The total distance between the sorted list values
+        int totalDistance = 0;
+
+        // List of counts that an item in list 1 appears in list 2
+        List<int> similarityScores = new List<int>();
+        // The total multiplication of this count of this item in list 1 found in list 2
+        int totalSimilarity = 0;
+
+        // Group the items by the unique values
+        var groupedItems = lists[1].GroupBy(j => j).ToList();
+        // Lists to make it super easy to access further down the line
+        List<int> groupOrder = new List<int>();
+        List<int> groupCounts = new List<int>();
+
+        // Iterate over the grouped items
+        foreach (var group in groupedItems)
+        {
+            // Store the unique value from the list here, we can 
+            // then get the index and retrieve the count in the second list
+            groupOrder.Add(group.Key);
+            // Store the count in this other list
+            groupCounts.Add(group.Count());
+        }
+
+        // Iterate over all items in list 1
+        for (int i = 0; i < lists[0].Count; i ++)
+        {
+            // Calculate the distance
+            int currentDistance = (int)MathF.Abs(lists[0][i] - lists[1][i]);
+            // Add it to our list of distances
+            distances.Add(currentDistance);
+            // Also add it to the running total distance
+            totalDistance += currentDistance;
+
+            // Check that this value exists in our grouped list
+            if (groupOrder.Contains(lists[0][i]))
+            {
+                // It does, so now lets get the value from the counts list
+                int similarityForItem = groupCounts.ElementAt(groupOrder.IndexOf(lists[0][i]));
+                // Add the similarity to our similarity list (just in case)
+                similarityScores.Add(lists[0][i] * similarityForItem);
+                // Addd it to the cumulative similarity too
+                totalSimilarity += (lists[0][i] * similarityForItem);
+            }
+        }
+
+
+        // Debug outputs
+        Console.WriteLine("List One: " + lists[0].Count);
+        Console.WriteLine("List Two: " + lists[1].Count);
+        Console.WriteLine("Total Distance: " + totalDistance);
+        Console.WriteLine("-------");
+        Console.WriteLine("Groups: " + groupOrder.Count);
+        Console.WriteLine("Group Counts: " + groupCounts.Count);
+        Console.WriteLine("Total Similarity: " + totalSimilarity);
+        Console.WriteLine("-------");
+        string listOne = "List One: ";
+        foreach (int item in lists[0])
+        {
+            listOne = listOne + item + ", ";
+        }
+        Console.WriteLine(listOne);
+        Console.WriteLine("-------");
+        string listTwo = "List Two: ";
+        foreach (int item in lists[1])
+        {
+            listTwo = listTwo + item + ", ";
+        }
+        Console.WriteLine(listTwo);
+        Console.WriteLine("-------");
+        string distancesList = "Distances: ";
+        foreach (int item in distances)
+        {
+            distancesList = distancesList + item + ", ";
+        }
+        Console.WriteLine(distancesList);
+    }
+}
+
