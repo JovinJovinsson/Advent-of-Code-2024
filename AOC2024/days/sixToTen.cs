f1f93f76b670f5055952ae46bb3e0352787f4975
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2024-12-07 00:57:06 +0700

    Comments done
    Have commented and moving onto Part 2

--- AOC2024/days/sixToTen.cs
+++ AOC2024/days/sixToTen.cs
@@ -16,9 +16,10 @@ public class SixToTen
         // The List<char> postion contains the X traversing (left/right)
         List<List<char>> map = new List<List<char>>();
 
-        // The initial starting point of the guard across both dimensions
+        // Used to track the position of the guard on the map
         int guardRow = -1;
         int guardColumn = -1;
+        // Our starting direction
         char guardFacing = '_';
 
         // Valid guard facings
@@ -38,29 +39,40 @@ public class SixToTen
                 // Extract the current line into a list of chars
                 map.Add(currentLine.ToCharArray().ToList());
 
+                // If we haven't yet found the guard, let's check if it's been found
                 if (guardColumn == -1)
                 {
+                    // Iterate over the valid facings
                     foreach (char facing in validFacings)
                     {
-                        if (guardColumn == -1 && currentLine.Contains(facing))
+                        // Check if the currentLine has the guard on it
+                        if (currentLine.Contains(facing))
                         {
+                            // It does, so grab the index in the List<char> (column)
                             guardColumn = currentLine.IndexOf(facing);
+                            // Grab the initial facing of the guard
                             guardFacing = facing;
+                            break;
                         }
                     }
 
+                    // Iterate the row each time if we haven't yet found the guard, and 1 last time after finding
                     guardRow++;
                 }
             }
         }
 
+        // Allows us to quit the do loop updating the map
         bool guardIsOnMap = true;
 
+        // Continually update the facings & location of the guard and check for whether on map
         do
         {
+            // Temporary stores that we can modify without damaging the originals
             int testRow = guardRow;
             int testColumn = guardColumn;
 
+            // Modify the row or column value based on the guards current facing
             switch (guardFacing)
             {
                 case '<': testColumn--; break;
@@ -70,31 +82,44 @@ public class SixToTen
                 default: break;
             }
 
+            // If both indices are within the bounds of the map, let's proceed
             if ((testRow >= 0 && testRow < map.Count) && (testColumn >= 0 && testColumn < map[0].Count))
             {
+                // If the next position for the guard would be obstructed let's rotate 90deg to the right
                 if (map[testRow][testColumn] == '#')
                 {
+                    // Get the index of the current facing and increment it, then modulo
+                    // by the count to ensure it wraps around to 0 when it's too high
                     int newFacingIndex = (validFacings.IndexOf(guardFacing) + 1) % validFacings.Count;
 
-                    // if (newFacingIndex >= validFacings.Count) { newFacingIndex = 0; }
-
+                    // Update the guard's facing
                     guardFacing = validFacings[newFacingIndex];
-                } else
+                }
+                // The next position isn't obstructed, so let's update our map & counts 
+                else
                 {
+                    // If the guard hasn't yet been in this position
                     if (map[guardRow][guardColumn] != 'X')
                     {
+                        // Mark it with an X
                         map[guardRow][guardColumn] = 'X';
+                        // Increment our distinct positions count
                         countOfPositions++;
                     }
                     
+                    // Update the current position of the guard to the new position
                     guardRow = testRow;
                     guardColumn = testColumn;
-
-                    // map[guardRow][guardColumn] = guardFacing;
                 }
-            } else
+            } 
+            // The guard has left the map
+            else
             {
+                // Flip our flag so we end the do loop
                 guardIsOnMap = false;
+                // Mark the last position before leaing with an X
+                map[guardRow][guardColumn] = 'X';
+                // Increment the number of positions one last time (the last position)
                 countOfPositions++;
             }
 
