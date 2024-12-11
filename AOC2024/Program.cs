711e06c479ada4ffd8f637e6b5bcfad2d5300799
JovinJovinsson <5206571+JovinJovinsson@users.noreply.github.com>
2024-12-11 07:00:06 +0000

    Bit of cleanup & prep for Day 11

--- AOC2024/Program.cs
+++ AOC2024/Program.cs
@@ -6,10 +6,39 @@
 // oneToFive.DayFour();
 // oneToFive.DayFive();
 
-SixToTen sixToTen = new SixToTen(); 
+// SixToTen sixToTen = new SixToTen(); 
 
 // sixToTen.DaySix();
 // sixToTen.DaySeven();
 // sixToTen.DayEight();
 // sixToTen.DayNine();
-sixToTen.DayTen();
+// sixToTen.DayTen();
+
+ElevenToFifteen elevenToFifteen = new ElevenToFifteen();
+
+#region Skeleton
+// #region Day Nine
+    // /// <summary>
+    // /// Solves the Day 9 portion of the AOC 2024 challenge.
+    // /// https://adventofcode.com/2024/day/9
+    // /// </summary>
+    // public void DayNine()
+    // {
+    //     // The input files
+    //     // string fileName = "assets/sixToTen/AOC2024.9.Test-Input copy.txt";
+    //     string fileName = "assets/sixToTen/AOC2024.9.Input.txt";
+
+    //     // Read the data in from the text file
+    //     using (StreamReader streamReader = new StreamReader(fileName))
+    //     {
+    //         // Placeholder for the current line of the tile
+    //         string currentLine;
+
+    //         // currentLine will be null when the StreamReader reaches the end of file
+    //         while((currentLine = streamReader.ReadLine()) != null)
+    //         {
+    //         }
+    //     }
+    // }
+    // #endregion
+#endregion
---
+++ AOC2024/days/elevenToFifteen.cs
@@ -0,0 +1,27 @@
+public class ElevenToFifteen
+{
+    #region Day Eleven
+    /// <summary>
+    /// Solves the Day 11 portion of the AOC 2024 challenge.
+    /// https://adventofcode.com/2024/day/11
+    /// </summary>
+    public void DayEleven()
+    {
+        // The input files
+        string fileName = "assets/elevenToFiften/AOC2024.11.Test-Input copy.txt";
+        // string fileName = "assets/elevenToFiften/AOC2024.11.Input.txt";
+
+        // Read the data in from the text file
+        using (StreamReader streamReader = new StreamReader(fileName))
+        {
+            // Placeholder for the current line of the tile
+            string currentLine;
+
+            // currentLine will be null when the StreamReader reaches the end of file
+            while((currentLine = streamReader.ReadLine()) != null)
+            {
+            }
+        }
+    }
+    #endregion
+}
